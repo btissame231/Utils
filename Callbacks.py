f947151db713265e47f2467ea6c197f00d764698
btissame231 <71394286+btissame231@users.noreply.github.com>
2022-05-30 04:19:51 +0200

    Update Callbacks.py

--- Callbacks.py
+++ Callbacks.py
@@ -1,6 +1,6 @@
 from keras.callbacks import ModelCheckpoint
 from keras_tqdm import TQDMNotebookCallback
-from tensorflow_addons.callbacks.cyclical_learning_rate import CyclicLR
+from tensorflow.keras_contrib.callbacks.cyclical_learning_rate import CyclicLR
 
 def get_callbacks(MODEL_PATH):
     """
